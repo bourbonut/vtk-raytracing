7631ce31fc2bd648b3b451e25e3fe91a1fa8ca48
bourbonut <ben.bourbon06@gmail.com>
2022-04-19 21:20:24 +0200

    Add a `setup.py` file

---
+++ setup.py
@@ -0,0 +1,23 @@
+# !/usr/bin/python3
+from setuptools import setup
+
+setup(
+    # package declaration
+    name="vtk-raytracing",
+    python_requires=">=3.7.11",
+    install_requires=[
+        "vtk>=9.1.0",
+        "numpy>=1.21.5",
+        "pyglm>=2.5.7",
+        "PyQt5>=5.15.6",
+        "rich-cli>=1.5.1",
+    ],
+    # metadata for pypi
+    license="GNU LGPL v3",
+    author="Benjamin Bourbon",
+    author_email="ben.bourbon06@gmail.com",
+    description="Raytracing using VTK",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bourbonut/vtk-raytracing",
+)
